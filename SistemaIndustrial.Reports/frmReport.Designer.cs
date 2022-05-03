0594f9a3550c7fd24e560355c0c01700c42b0e03
MARCOS GARCIA MACHADO <marcosgarciarv@gmail.com>
2022-05-03 14:50:50 -0300

    Reports em .NetFramework

--- SistemaIndustrial.Reports/frmReport.Designer.cs
+++ SistemaIndustrial.Reports/frmReport.Designer.cs
@@ -30,38 +30,140 @@ namespace SistemaIndustrial.Reports
         /// </summary>
         private void InitializeComponent()
         {
-            MessageBox.Show("1");
+            this.components = new System.ComponentModel.Container();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dsv_SisIndDataSet1 = new SistemaIndustrial.Reports.dsv_SisIndDataSet1();
+            this.vwCompraGadoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.vwCompraGadoTableAdapter = new SistemaIndustrial.Reports.dsv_SisIndDataSet1TableAdapters.vwCompraGadoTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataEntregaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsv_SisIndDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vwCompraGadoBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
-            //
-            MessageBox.Show("2");
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            // 
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "SistemaIndustrial.Reports.Reports.ReportCompraGado.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Location = new System.Drawing.Point(0, 149);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(800, 641);
+            this.reportViewer1.Size = new System.Drawing.Size(800, 560);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.dataEntregaDataGridViewTextBoxColumn,
+            this.totalDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.vwCompraGadoBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(742, 96);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(287, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LISTA DE COMPRAS DE GADO";
+            // 
+            // dsv_SisIndDataSet1
+            // 
+            this.dsv_SisIndDataSet1.DataSetName = "dsv_SisIndDataSet1";
+            this.dsv_SisIndDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // vwCompraGadoBindingSource
+            // 
+            this.vwCompraGadoBindingSource.DataMember = "vwCompraGado";
+            this.vwCompraGadoBindingSource.DataSource = this.dsv_SisIndDataSet1;
+            // 
+            // vwCompraGadoTableAdapter
+            // 
+            this.vwCompraGadoTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id (Cód.)";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Pecuarista";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            this.nomeDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // dataEntregaDataGridViewTextBoxColumn
+            // 
+            this.dataEntregaDataGridViewTextBoxColumn.DataPropertyName = "DataEntrega";
+            this.dataEntregaDataGridViewTextBoxColumn.HeaderText = "Data de Entrega";
+            this.dataEntregaDataGridViewTextBoxColumn.Name = "dataEntregaDataGridViewTextBoxColumn";
+            this.dataEntregaDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // totalDataGridViewTextBoxColumn
+            // 
+            this.totalDataGridViewTextBoxColumn.DataPropertyName = "Total";
+            this.totalDataGridViewTextBoxColumn.HeaderText = "Total (R$)";
+            this.totalDataGridViewTextBoxColumn.Name = "totalDataGridViewTextBoxColumn";
+            this.totalDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // frmReport
             // 
-            MessageBox.Show("3");
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.ClientSize = new System.Drawing.Size(800, 709);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.reportViewer1);
             this.Name = "frmReport";
-            this.Text = "Form1";
+            this.Text = "Lista de Compra de Gado";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsv_SisIndDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vwCompraGadoBindingSource)).EndInit();
             this.ResumeLayout(false);
-            MessageBox.Show("4");
+            this.PerformLayout();
+
         }
 
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private DataGridView dataGridView1;
+        private Label label1;
+        private dsv_SisIndDataSet1 dsv_SisIndDataSet1;
+        private BindingSource vwCompraGadoBindingSource;
+        private dsv_SisIndDataSet1TableAdapters.vwCompraGadoTableAdapter vwCompraGadoTableAdapter;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn dataEntregaDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn totalDataGridViewTextBoxColumn;
     }
 }
 
--- SistemaIndustrial.Reports/frmReport.cs
+++ SistemaIndustrial.Reports/frmReport.cs
@@ -18,12 +18,11 @@ namespace SistemaIndustrial.Reports
 {
     public partial class frmReport : Form
     {
-
         private int _IdCompra;
-
         private CompraGadoViewModel _compraGadoCabecalho;
         private readonly static string _connectionString = ConfigurationManager.ConnectionStrings["SisIndConnectionString"].ConnectionString;
         private List<CompraGadoItemViewModel> _listItens;
+
         public frmReport()
         {
             InitializeComponent();
@@ -34,6 +33,11 @@ namespace SistemaIndustrial.Reports
             _IdCompra = idCompra;
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'dsv_SisIndDataSet1.vwCompraGado'. Você pode movê-la ou removê-la conforme necessário.
+            this.vwCompraGadoTableAdapter.Fill(this.dsv_SisIndDataSet1.vwCompraGado);
+        }
         private void CriarReportCompraGado()
         {
             BuscarCabecalhoCompra();
@@ -52,11 +56,6 @@ namespace SistemaIndustrial.Reports
             reportViewer1.LocalReport.SetParameters(parameters);
             reportViewer1.LocalReport.Refresh();
             reportViewer1.RefreshReport();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            CriarReportCompraGado();
-            this.reportViewer1.RefreshReport();
         }
         private bool BuscarCabecalhoCompra()
         {
@@ -78,7 +77,6 @@ namespace SistemaIndustrial.Reports
                     _compraGadoCabecalho.Pecuarista = reader.GetString(1);
                     _compraGadoCabecalho.DataEntrega = reader.GetDateTime(2);
                     _compraGadoCabecalho.Total = reader.GetDecimal(3);
-
                 }
 
                 return true;
@@ -96,8 +94,7 @@ namespace SistemaIndustrial.Reports
                 command.ExecuteNonQuery();
 
                 SqlDataReader reader = command.ExecuteReader();
-                if (_listItens == null)
-                    _listItens = new List<CompraGadoItemViewModel>();
+                _listItens = new List<CompraGadoItemViewModel>();
                 
                 while (reader.Read())
                 {
@@ -106,6 +103,7 @@ namespace SistemaIndustrial.Reports
                     //itemCompra.IdCompraGado = int.Parse(reader["IdCompraGado"].ToString());
                     itemCompra.Preco = decimal.Parse(reader["Preco"].ToString()); 
                     itemCompra.Descricao = reader["Descricao"].ToString();
+                    itemCompra.Quantidade = int.Parse(reader["Quantidade"].ToString());
                     itemCompra.Total = decimal.Parse(reader["Total"].ToString());
                     
                     _listItens.Add(itemCompra);
@@ -113,6 +111,12 @@ namespace SistemaIndustrial.Reports
 
                 return true;
             }
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            _IdCompra = Int32.Parse(dataGridView1.SelectedCells[0].Value.ToString());
+            CriarReportCompraGado();
+            this.reportViewer1.RefreshReport();
         }
     }
 }
